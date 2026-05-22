b3fc012cdea7118ecef9764aa819f05a4dee03c1
ArthurDaniel02 <arthurd4niel02@gmail.com>
2026-05-22 16:47:51 -0300

    IA2

--- questoes/views.py
+++ questoes/views.py
@@ -142,7 +142,7 @@ class GerarQuestaoIAView(APIView):
             client = genai.Client(api_key=chave_api)
             
             resposta = client.models.generate_content(
-                model='gemini-1.5-flash',
+                model='gemini-2.0-flash',
                 contents=system_prompt,
                 config=types.GenerateContentConfig(
                     response_mime_type="application/json",
