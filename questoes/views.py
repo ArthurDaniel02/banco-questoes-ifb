1cebc4bc124dc930c6796ac81d6062e09bb2f6e9
ArthurDaniel02 <arthurd4niel02@gmail.com>
2026-06-15 13:26:39 -0300

    fix groq

--- questoes/views.py
+++ questoes/views.py
@@ -148,7 +148,7 @@ class GerarQuestaoIAView(APIView):
                         "content": system_prompt
                     }
                 ],
-                model="llama3-8b-8192",
+                model="llama-3.1-8b-instant",
                 temperature=0.7,
             
                 response_format={"type": "json_object"}, 
